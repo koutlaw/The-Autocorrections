195322073dc83d884eef7a7b296aad2a6a2b9512
koutlaw <kyle.outlaw@gmail.com>
2014-03-11 14:40:21 -0400

    Update codefreeze.py

--- project_code/codefreeze.py
+++ project_code/codefreeze.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 # My first python program
-print "Hello, World!\n"
+print "Hey tiles Paul, I'm calling in response to the 88. You know that you said, and hey man, it's a conversation. And you know. I'll send you some links. I don't know how. Hopefully I'll be. But depending on what your objectives are. And you know it may be more useful to like I said talk and figure out. I think what your next steps would be. I'm gonna be around most of the day. I'm actually on T T O from tomorrow on through next week, so if you wanna call me today, that'd be good to do it. Take care.\n"
