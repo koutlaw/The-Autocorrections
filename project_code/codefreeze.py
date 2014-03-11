fa713030ed701e2a11cf98f616c927ac934c8e86
koutlaw <kyle.outlaw@gmail.com>
2014-03-11 14:39:09 -0400

    Update codefreeze.py

--- project_code/codefreeze.py
+++ project_code/codefreeze.py
@@ -1,16 +1,3 @@
-#!//usr/bin/python
-#Story generation by Nick Montfort
-#Story by J.R. Carpenter
-#2009-06-24
-#Thanks to Ingrid & Pookie
-from random import choice
-while True:
- s=['There was a knock at the door It was Edwin, dressed in black'
-'My god you look like crap today said Emma who was attempting to remove a coffee stain from her chemise'
-
-]
- l=choice(range(5,10))
- while len(s)>l:
-  s.remove(choice(s))
- print "\nCODEFREEZE:\n"+'.\n'.join(s)+'.'
- raw_input('To be continued...')
+#!/usr/bin/python
+# My first python program
+print "Hello, World!\n"
